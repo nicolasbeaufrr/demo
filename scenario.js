5c6c302ec32958cdbe76c2b56378d6a9d09fe34c
nicolasbeaufrr <111557265+nicolasbeaufrr@users.noreply.github.com>
2022-08-19 07:51:22 +0200

    Update scenario.js

--- scenario.js
+++ scenario.js
@@ -1,7 +1,1 @@
-async (page) => {
-  // Go to the url passed to the command line (see below)
-  await page.goto("", { waitUntil: "networkidle" });
-  await page.waitForTimeout(3000); // Wait for 3 seconds
-  await page.scrollToElement("footer"); // Scroll to the bottom of the page (change the element name depending on your markup)
-  await page.waitForNetworkIdle(); // Wait for every request to be answered - like a normal user would do
-};
+
